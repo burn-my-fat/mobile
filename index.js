34199be48299050d873e2121ddf2dd133ba304a0
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-08-11 01:04:39 +0300

    Prepare project for development :grimacing:

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, View, Text } from 'react-native';
 import {name as appName} from './app.json';
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Text>Burn My Fat!</Text>
+  </View>
+)
 
 AppRegistry.registerComponent(appName, () => App);
